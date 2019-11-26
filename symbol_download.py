6a0446c6cd9e3f8e546107f9472e56559321161d
fro391 <richardguo0122@gmail.com>
2019-11-25 20:10:24 -0500

    Update symbol_download.py

--- symbol_download.py
+++ symbol_download.py
@@ -64,7 +64,7 @@ def nasdaq_ticker_downloader():
                     pass
             len2 = len(symbolslist)
 
-            with open ('symbols_alt.txt','w+') as symbolfile:             #write to old file
+            with open ('symbols.txt','w+') as symbolfile:             #write to old file
                 counter = 0 #counter for not adding '\n' at the last line
                 for s in symbolslist:
                     counter += 1
@@ -75,9 +75,63 @@ def nasdaq_ticker_downloader():
             print ("Added", len2-len1, "stocks")
         except:
             pass
+
+def nasdaq_ticker_analyzer():
+    dt = datetime.datetime.now()
+    UnixTime = int(time.mktime(dt.timetuple()))
+    symbolslist = []
+    #proxies
+    http_proxy  = ''
+    https_proxy = ''
+    ftp_proxy   = ''
+    proxyDict = { 
+                  "http"  : http_proxy, 
+                  "https" : https_proxy, 
+                  "ftp"   : ftp_proxy
+                }
+
+    hdr = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+       'Connection': 'keep-alive'}
+    
+    for i in range(350):
+        try:
+            start_pos = i+1
+            url = 'https://www.nasdaq.com/api/v1/screener?page={}&pageSize=20'.format(start_pos)
+            resp = requests.get(url, headers=hdr)
+            if resp.status_code != 200:
+                # This means something went wrong.
+                raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+            data = json.loads(resp.text)
+            for j in range(19):
+                try:
+                    if data['data'][j]['bloggerSentimentData']['signal'] == 'Bullish' and data['data'][j]['newsSentimentData']['score'] >= 0.8:#data['data'][j]['newsSentimentData']['score'] >= 0.5 and data['data'][j]['insiderSentimentData']['score'] >= 0.5 and data['data'][j]['mediaBuzzData']['score'] >= 0.5 and data['data'][j]['hedgeFundSentimentData']['score'] >= 0.5 and data['data'][j]['investorSentimentData']['score'] >= 0.5 and data['data'][j]['bloggerSentimentData']['signal'] == 'Bullish': 
+                        symbolslist.append(data['data'][j]['ticker'])
+                        print(data['data'][j]['ticker'])
+                        print ('newsSentimentData', data['data'][j]['newsSentimentData'])
+                        print ('insiderSentimentData', data['data'][j]['insiderSentimentData'])
+                        print ('mediaBuzzData', data['data'][j]['mediaBuzzData'])
+                        print ('hedgeFundSentimentData', data['data'][j]['hedgeFundSentimentData'])
+                        print ('investorSentimentData', data['data'][j]['investorSentimentData'])
+                        print ('bloggerSentimentData', data['data'][j]['bloggerSentimentData'])
+                except:
+                    pass
+        except:
+            pass
+    #add symbols to file
+        with open ('symbols_nasdaq.txt','w+') as symbolfile:             
+            counter = 0 #counter for not adding '\n' at the last line
+            for s in symbolslist:
+                counter += 1
+                if counter < len(symbolslist):
+                    symbolfile.write(str(s)+ '\n')
+                else:
+                    symbolfile.write(str(s))
+
         
 if __name__ == '__main__':
     #start timer
     start = timeit.default_timer()
-    
     nasdaq_ticker_downloader()
+    nasdaq_ticker_analyzer()
+    
